2596e0288131d6ea686a4c6ef831ee18b70b8162
Gunnar Marquardt Desktop <gunnargmarquardt@gmail.com>
2019-09-07 12:54:16 -0600

    Finished first assignment

--- 280_Arrays1_GMarquardt/280_Arrays1_GMarquardt/280_Arrays1_GMarquardt.cpp
+++ 280_Arrays1_GMarquardt/280_Arrays1_GMarquardt/280_Arrays1_GMarquardt.cpp
@@ -10,6 +10,11 @@ using namespace std;
 
 void load(int a[], int c)
 {
+	if (c <= 0 || c > 10)
+	{
+		cout << "Invalid input" << endl;
+		return;
+	}
 	for (int i = 0; i < c; i++) 
 	{
 		cout << "Enter number " << i + 1 << ":" << endl;
@@ -20,20 +25,98 @@ void load(int a[], int c)
 void display(int a[], int c)
 {
 	cout << "{ ";
-	for (int i = 0; i < c; i++) {
+	for (int i = 0; i < c; i++) 
+	{
 		cout << a[i] << " ";
 	}
 	cout << "}" << endl;
 }
 
-int get(int a[], int n)
+int get(int a[], int c, int n)
 {
-	return a[n];
+	if (n > c - 1)
+	{
+		return NULL;
+	}
+	else
+	{
+		return a[n];
+	}
+}
+
+void set(int a[], int c, int n, int v) 
+{
+	a[n] = v;
+}
+
+void deleteElement(int a[], int& c, int n) 
+{
+	for (int i = n; i < c - 1; i++) 
+	{
+		a[i] = a[i + 1];
+	}
+	a[c - 1] = 0;
+	c -= 1;
+}
+
+void insert(int a[], int& c, int n, int v) 
+{
+	if (c < 10)
+	{
+		c += 1;
+	}
+	for (int i = c - 1; i > n; i--) 
+	{
+		a[i] = a[i - 1];
+	}
+	a[n] = v;
+}
+
+int findMin(int a[], int c)
+{
+	int min = a[0];
+	for (int i = 1; i < c; i++) 
+	{
+		if (a[i] < min)
+		{
+			min = a[i];
+		}
+	}
+	return min;
+}
+
+int findAve(int a[], int c)
+{
+	double ave;
+	double total = 0.0;
+	for (int i = 0; i < c; i++)
+	{
+		total += a[i];
+	}
+	ave = total / c;
+	return ave;
+}
+
+void sort(int a[], int c)
+{
+	int temp;
+	for (int pass = 0; pass < c - 1; pass++)
+	{
+		for (int pos = 0; pos < c - pass - 1; pos++)
+		{
+			if (a[pos] > a[pos + 1])
+			{
+				temp = a[pos];
+				a[pos] = a[pos + 1];
+				a[pos + 1] = temp;
+			}
+		}
+	}
 }
 
 int main()
 {
-	int array[] = {0,0,0,0,0,0,0,0,0,0}, choice, count = 0;
+	int array[] = {0,0,0,0,0,0,0,0,0,0}, choice, count = 0, n, v;
 	bool done = false;
 	do
 	{
@@ -53,33 +136,76 @@ int main()
 		switch (choice)
 		{
 		case 1:
-			int check;
 			cout << "How many?" << endl;
-			cin >> check;
-			if (check > 0 && check <= 10)
-			{
-				load(array, count);
-			}
-			else
-			{
-				cout << "Invalid input" << endl;
-			}
+			cin >> count;
+			load(array, count);
 			break;
 		case 2:
 			display(array, count);
 			break;
 		case 3:
-			int n;
-			cout << " Which element?" << endl;
+			cout << "Which element?" << endl;
+			cin >> n;
+			if (get(array, count, n) != NULL) {
+				cout << get(array, count, n) << endl;
+			}
+			else
+			{
+				cout << "Invalid Element" << endl;
+			}
+			break;
+		case 4:
+			cout << "Which element?" << endl;
+			cin >> n;
+			cout << "What value?" << endl;
+			cin >> v;
+			if (n > count - 1)
+			{
+				cout << "Invalid element" << endl;
+				break;
+			}
+			set(array, count, n, v);
+			display(array, count);
+			break;
+		case 5:
+			cout << "Which element?" << endl;
 			cin >> n;
 			if (n > count - 1)
 			{
 				cout << "Invalid element" << endl;
+				break;
 			}
-			else
+			deleteElement(array, count, n);
+			display(array, count);
+			break;
+		case 6:
+			cout << "Which element?" << endl;
+			cin >> n;
+			cout << "What value?" << endl;
+			cin >> v;
+			if (n > count - 1)
 			{
-				get(array, n);
+				cout << "Invalid element" << endl;
+				break;
 			}
+			insert(array, count, n, v);
+			display(array, count);
+			break;
+		case 7:
+			cout << findMin(array, count) << endl;
+			break;
+		case 8:
+			cout << findAve(array, count) << endl;
+			break;
+		case 9:
+			sort(array, count);
+			display(array, count);
+			break;
+		case 10:
+			done = true;
+			break;
+		default:
+			cout << "Invalid input" << endl;
 			break;
 		}
 	} while (done == false);
